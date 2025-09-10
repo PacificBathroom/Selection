fe637041f82d22e3be2ceeb36d3fcfd2e19c7d2a
PacificBathroom <chloejaysheppard@gmail.com>
2025-09-10 13:17:25 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Netlify expects /dist (per your netlify.toml)
+    sourcemap: false, // you can set true if you want source maps
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
