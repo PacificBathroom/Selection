3c67851a5c8a626dcd72d4ef25eab1ff465eeb93
PacificBathroom <chloejaysheppard@gmail.com>
2025-09-18 18:11:18 +1000

    Create pdfjs-worker.d.ts

---
+++ src/types/pdfjs-worker.d.ts
@@ -0,0 +1,9 @@
+// src/types/pdfjs-worker.d.ts
+declare module 'pdfjs-dist/build/pdf.worker.min.mjs' {
+  const url: string;
+  export default url;
+}
+declare module 'pdfjs-dist/build/pdf.worker.mjs' {
+  const url: string;
+  export default url;
+}
