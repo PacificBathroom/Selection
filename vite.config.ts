89662b71ef6c1f82309fd2e2f737a9a1fef9e0fa
PacificBathroom <chloejaysheppard@gmail.com>
2025-09-18 22:21:51 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath, URL } from "node:url";
-import path from "path";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: "public", // ensure Excel file gets copied to dist
   resolve: {
     alias: {
-     "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
+});
+
   build: {
     outDir: "dist",
     sourcemap: true,
