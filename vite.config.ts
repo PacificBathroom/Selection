bca8402910ad854b774e01e7b8d1883e8188a849
PacificBathroom <chloejaysheppard@gmail.com>
2025-09-18 17:11:54 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath, URL } from "node:url";
 import path from "path";
 
 export default defineConfig({
